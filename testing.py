92aa506391e1003be4dce2144115fd4b94149f39
Luna Astris <84162680+LunaAstris16@users.noreply.github.com>
2022-02-18 00:38:51 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,18 @@
-print("new thing")
+import os
+import subprocess
+
+def foldernamefinder(githuburl):
+    githuburl.split("/")
+    print(githuburl)
+
+githuburl = "https://github.com/LunaAstris16/testing.git"
+folderName = foldernamefinder(githuburl)
+
+#subprocess.run("git", "clone", str(githuburl))
+
+#lsoutput = os.popen("ls").read().split("\n")
+#for i in range(len(lsoutput)):
+#    if lsoutput == 
+
+#while True:
+#    os.chdir()
